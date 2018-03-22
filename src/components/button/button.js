8b6e921f77e8f3228e9a0bad723a516d2a286b31
WebDevLyna <t.lyna.nguyen@gmail.com>
2018-03-21 23:16:03 -0400

    Styles

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './button.css';
 
 export const Button = () => (
-  <button>
+  <button className="likeContainer">
     <img className="likeBtn" src="http://www.pngall.com/wp-content/uploads/2016/04/Instagram-Heart-Transparent.png" alt="like" onClick={this.myFunction} />
   </button>
 );
--- src/components/paragraph.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './paragraph.css';
-
-export const Paragraph = () => (
-  <p>This is a paragraph</p>
-);
